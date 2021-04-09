fe67883fba3d47440c56f70829fa1cf55394b92c
Daniel Cisneros <DANDANUS1999@gmail.com>
2021-04-09 11:29:56 -0300

    Updates

---
+++ precache-manifest.723fbfed5a4385ec9915be4328bbaaff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ceff6e3d41051de8e6a58094d67ce3",
+    "url": "/rickandmorty-favs/index.html"
+  },
+  {
+    "revision": "e9faad9300d632cf1f17",
+    "url": "/rickandmorty-favs/static/css/2.79a35eaf.chunk.css"
+  },
+  {
+    "revision": "a1899b7c62896f16ba1d",
+    "url": "/rickandmorty-favs/static/css/main.ab8d5142.chunk.css"
+  },
+  {
+    "revision": "e9faad9300d632cf1f17",
+    "url": "/rickandmorty-favs/static/js/2.c7a679d8.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/rickandmorty-favs/static/js/2.c7a679d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1899b7c62896f16ba1d",
+    "url": "/rickandmorty-favs/static/js/main.7b40dbb5.chunk.js"
+  },
+  {
+    "revision": "ecc6bc985e8957f934eb",
+    "url": "/rickandmorty-favs/static/js/runtime-main.e3719bad.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rickandmorty-favs/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rickandmorty-favs/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rickandmorty-favs/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rickandmorty-favs/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rickandmorty-favs/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
